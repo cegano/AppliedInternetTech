42c335194471a4d95ce684afd2421419bb343144
cegano <118409496+cegano@users.noreply.github.com>
2023-11-27 16:31:03 -0500

    Program 4
    Environment files not included

--- app.py
+++ app.py
@@ -1,29 +1,36 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash, url_for
 from models import *
+
 app = Flask(__name__)
 
-app.config ['SQLALCHEMY_DATABASE_URI'] ="postgresql://postgres:Postgresql@localhost:5432/CIT109"
-app.config ["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config ['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Postgresql@localhost:5432/library"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
-@app.route("/")
+@app.route("/index", methods = ["GET", "POST"])
 def index():
-    stu = ruby_on_rails.query.all()
-    return render_template("index.html", stu=stu, heading='Students Data')
-
-@app.route("/major", methods=['GET', 'POST'])
-def by_major():
-    if request.method=='GET':
-        return render_template("majorForm.html")
+    if request.method == "GET":
+        return render_template("index.html")
     else:
-        major = request.form.get('major')
-        stu = ruby_on_rails.query.filter_by(major=major).all()
-        return render_template("index.html", stu=stu, heading='Students by Major')
+        aid = request.form.get('author_id')
+        author = Authors.query.get(aid)
+        return render_template("findAuthor.html", author=author)
+
+@app.route("/findBooks", methods = ["GET", "POST"])
+def findBooks():
+    books = Books.query.all()
+    return render_template("findBooks.html", books=books)
+
+@app.route("/showBook")
+def showBook():
+    bid = request.args.get('bid')
+    book = Books.query.get(bid)
+    return render_template("titleForm.html", book=book)
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404_not_found.html'), 404
+    return render_template('404_error.html', title='error'), 404
 
 if __name__ == "__main__":
     db.create_all()
--- models.py
+++ models.py
@@ -1,11 +1,19 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class ruby_on_rails(db.Model):
-    __tablename__ = "ruby_on_rails"
-    id = db.Column(db.Integer, primary_key=True)
+class Authors(db.Model):
+    __tablename__ = "authors"
+    aid = db.Column(db.Integer, primary_key=True)
     last_name = db.Column(db.String, nullable=False)
-    hrs_attempted = db.Column(db.Integer, nullable=False)
-    gpa_points = db.Column(db.Float, nullable=False)
-    major = db.Column(db.String, nullable=False)
+    first_name = db.Column(db.String, nullable=False)
+    year_born = db.Column(db.Integer, nullable=False)
+    books = db.relationship("Books", backref="authors")
+
+class Books(db.Model):
+    __tablename__ = "books"
+    bid = db.Column(db.Integer, primary_key=True)
+    aid = db.Column(db.Integer, db.ForeignKey('authors.aid'), nullable=False)
+    title = db.Column(db.String, nullable=False)
+    year_published = db.Column(db.Integer, nullable=False)
